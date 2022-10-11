c308201b667e51ad84fd7f5547b56c21bdbbff55
B9P31004 <55864437+B9P31004@users.noreply.github.com>
2022-10-11 17:44:02 +0900

    first commit

---
+++ opennlp_api/opennlp_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpenNlpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'opennlp_app'
---
+++ opennlp_api/opennlp_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http.response import HttpResponse
+from nltk_opennlp.taggers import OpenNLPTagger
+
+def opennlp_api(request):
+    if request.method == "GET":
+        return HttpResponse()
+    else:
+        phrase=request.body.decode()
+        path_to_bin='/apache-opennlp/bin'
+        path_to_model='/opennlp_models/en-pos-maxent.bin'
+        tt = OpenNLPTagger(language='en',path_to_bin=path_to_bin,path_to_model=path_to_model)
+        sentence = tt.tag(phrase)
+        print(sentence)
+    return HttpResponse(sentence)
